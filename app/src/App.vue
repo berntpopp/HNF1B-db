e61c444578051f6400987034d1c1d08f42a6da85
berntpopp <bernt.popp@googlemail.com>
2021-09-18 13:54:25 +0200

    initial page design

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,55 +1,84 @@
 <template>
-  <v-app>
+  <v-app id="HNF1B-db">
     <v-app-bar
       app
-      color="primary"
-      dark
+      color="white"
+      flat
+      dense
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      <v-tabs
+        class="ml-n9"
+        color="grey darken-1"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+
+        <v-tab
+        >
+          <img
+            src="../public/HNF1B-db_logo.png"
+            height="40px"
+          />
+        </v-tab>
+
+        <v-tab
+          v-for="link in links"
+          :key="link"
+        >
+          {{ link }}
+        </v-tab>
+      </v-tabs>
+
     </v-app-bar>
 
-    <v-main>
-      <router-view/>
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            sm="2"
+          >
+            <v-sheet
+              rounded="lg"
+              min-height="70vh"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col
+            cols="12"
+            sm="10"
+          >
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
+
 <script>
-
-export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+  export default {
+    data: () => ({
+      links: [
+        'Tables',
+        'Analyses',
+        'Search',
+        'About',
+      ],
+    }),
+  }
 </script>
+
+<style>
+.v-app-bar-title__content{
+  width: 200px !important;
+}
+</style>
