91892e0b216a95ce22a426fd7d9563f69d36f478
berntpopp <bernt.popp@googlemail.com>
2022-07-13 22:40:41 +0200

    add JWT based user authentication

--- api/start_api.R
+++ api/start_api.R
@@ -5,4 +5,11 @@ Sys.setenv(API_CONFIG = "hnf1b_db")
 setwd("/hnf1b_api_volume")
 
 root <- pr("api_plumber.R") %>%
+        pr_set_api_spec(function(spec) {
+        spec$components$securitySchemes$bearerAuth$type <- "http"
+        spec$components$securitySchemes$bearerAuth$scheme <- "bearer"
+        spec$components$securitySchemes$bearerAuth$bearerFormat <- "JWT"
+        spec$security[[1]]$bearerAuth <- ""
+        spec
+        })  %>%
         pr_run(host = "0.0.0.0", port = 7779)
--- api/start_api_development.R
+++ api/start_api_development.R
@@ -5,4 +5,11 @@ Sys.setenv(API_CONFIG = "hnf1b_db_local")
 setwd("C:/development/HNF1B-db/api")
 
 root <- pr("api_plumber.R") %>%
+        pr_set_api_spec(function(spec) {
+        spec$components$securitySchemes$bearerAuth$type <- "http"
+        spec$components$securitySchemes$bearerAuth$scheme <- "bearer"
+        spec$components$securitySchemes$bearerAuth$bearerFormat <- "JWT"
+        spec$security[[1]]$bearerAuth <- ""
+        spec
+        })  %>%
         pr_run(host = "0.0.0.0", port = 7779)
