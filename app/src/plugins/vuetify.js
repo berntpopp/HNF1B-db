a9fb3b659f0e6f3c8b0ab26f5df3b5784dabec5b
berntpopp <bernt.popp@googlemail.com>
2022-07-15 15:53:51 +0200

    mdi font settings

--- app/src/plugins/vuetify.js
+++ app/src/plugins/vuetify.js
@@ -1,7 +1,13 @@
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-});
+    icons: {
+      iconfont: 'mdi', // default - only for display purposes
+    },
+  });
