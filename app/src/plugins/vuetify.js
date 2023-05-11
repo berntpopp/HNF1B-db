02a5efd9e1f99c7835c61464ac78cd260a1b9c38
berntpopp <bernt.popp@googlemail.com>
2023-05-11 17:45:25 +0200

    add nonce

--- app/src/plugins/vuetify.js
+++ app/src/plugins/vuetify.js
@@ -7,4 +7,7 @@ export default new Vuetify({
     icons: {
       iconfont: 'mdiSvg', // default - only for display purposes
     },
+    theme: {
+      options: { cspNonce: 'dQw4w9WgXcQ' },
+    },
   });
